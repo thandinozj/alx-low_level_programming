875dcce8edbd9efa4f426e928ee45c475d5ed534
Thandeka <thandekajele@gmail.com>
2023-02-16 03:44:46 -0800

    advancedtask8commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,10 @@
 #include <unistd.h>
 /**
  * main - C program that prints a line to the standard error
- * Return 1 (Always success)
+ * Return: 1 (Always success)
  */
 int main(void)
+
 {
 write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
