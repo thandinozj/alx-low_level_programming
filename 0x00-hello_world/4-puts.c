b24e696cff1b8444cedb185cb4eb2d6cca3d4960
Thandeka <thandekajele@gmail.com>
2023-02-16 02:33:13 -0800

    task4commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program prints a line with puts function
+ * Return: 0 (Always)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
