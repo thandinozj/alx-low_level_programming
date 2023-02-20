750751c6308556aadc10dc305355bbcb0c0a30f0
Thandeka <thandekajele@gmail.com>
2023-02-20 05:22:42 -0800

    task0commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,11 @@ printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is positive\n", n);
+printf("%d is negative\n", n);
 }
 else
 {
-printf("%d is positive\n", n);
+printf("%d is zero\n", n);
 }
 return (0);
 }
