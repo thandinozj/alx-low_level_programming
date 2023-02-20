5185b09c672069f0cda91fc1283ff288c9c325c2
Thandeka <thandekajele@gmail.com>
2023-02-20 06:18:42 -0800

    task1and deletioncommit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,8 +3,8 @@
 #include <time.h>
 
 /**
- * main - Entry
- * Return: Always 0
+ * main - assign random number to the variable each time it is executed
+ * Return: Always 0 (sucess)
  */
 int main(void)
 {
@@ -19,11 +19,11 @@ printf("Last digit of %d is %d and is greater than 5\n", n, a);
 }
 else if (a == 0)
 {
-printf("Last digit of %d is %d and is than 0\n", n, a);
+printf("Last digit of %d is %d and is 0\n", n, a);
 }
 else
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 }
-return (0)
+return (0);
 }
