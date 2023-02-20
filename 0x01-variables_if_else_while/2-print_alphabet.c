4cd1ce8582567520a9c4039ad65522bfc0b92c51
Thandeka <thandekajele@gmail.com>
2023-02-20 06:30:27 -0800

    task2commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lower case
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+char lc;
+for (lc = 'a'; lc <= 'z'; lc++)
+{
+putchar(lc);
+}
+putchar('\n');
+return (0);
+}
