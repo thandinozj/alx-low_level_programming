6be1a45fb299a02535a5db81a953cf6621276e03
Thandeka <thandekajele@gmail.com>
2023-02-27 12:04:33 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,18 +6,10 @@
  */
 void print_rev(char *s)
 {
-int longi = 0;
-int o;
-while (*s != '\0')
-{
-longi++;
-s++;
-}
-s = =;
-for (o = longi; o > 0; o--)
-{
-_putchar(*s);
-s--;
-}
+int i = 0;
+while (s[i] != '\0')
+i++;
+for (i = i - 1; i >= 0; i--)
+_putchar(s[i]);
 _putchar('\n');
 }
