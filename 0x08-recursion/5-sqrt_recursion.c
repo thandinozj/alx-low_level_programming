086bb5c81382eea9e687b5db1444b1a70c8763ac
Thandeka <thandekajele@gmail.com>
2023-03-08 09:29:25 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,3 +17,18 @@ return (i);
 return (-1);
 return (-1);
 }
+/**
+ * _sqrt_recursion - evaluate sqrt
+ * @n: interger
+ * Return: sqrt_recursion
+ */
+int _sqrt_recursion(int n)
+{
+int i = 0;
+if (i < 0) /*If n is negative*/
+return (-1);
+else
+{
+return (_evaluate(i, n)); /*Recursive call*/
+}
+}
