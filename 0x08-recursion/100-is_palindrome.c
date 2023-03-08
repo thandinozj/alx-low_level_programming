5ebafa9b9742682fd65c19b90afadd951e62984f
Thandeka <thandekajele@gmail.com>
2023-03-08 09:54:55 -0800

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: recursiion
+ */
+int _strlen_recursion(char *s)
+{
+if (!*s)
+{
+return (0);
+}
+return (1 + _strlen_recursive(+ss));
+}
+/**
+ * p1 - palindrome
+ * @s: pointer to string
+ * @l: position
+ * Return: boolena
+ */
+int p1(char *s, int l)
+{
+if (l < 1)
+{
+return (1);
+}
+if (*s == *(s + l))
+{
+return (p1(s + 1, l - 2));
+}
+return (0);
+}
