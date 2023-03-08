a7ab5c43cb8476971649f23c6c5b4db01b6e83ac
Thandeka <thandekajele@gmail.com>
2023-03-08 09:21:26 -0800

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,9 +7,9 @@
 int factorial(int n)
 {
 if (n == 0) /*Base condition*/
-{
 return (1);
-}
 else if (n < 0) /*Base condition*/
+return (-1);
+else
 return (n * factorial(n - 1)); /*Recursive call*/
 }
