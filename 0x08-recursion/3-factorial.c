7e6805f0e1379cfc49b0725a27305af3287325ec
Thandeka <thandekajele@gmail.com>
2023-03-08 09:08:17 -0800

    5-sqrt_recursion.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * factorial - factorial of a given number
+ * @n: pointer block of a memory to fill
+ * Return: factorial
+ */
+int factorial(int n)
+{
+if (n == 0) /*Base condition*/
+return (1);
+else if (n < 0) /*Base condition*/
+return (n * factorial(n - 1)); /*Recursive call*/
+}
